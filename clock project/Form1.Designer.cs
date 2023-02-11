03533a9a35176e6b43a51d44f24be89b81f7c4e3
ashwagMohsen <79316100+ashwagMohsen@users.noreply.github.com>
2023-02-11 06:28:28 +0300

    Add files via upload

---
+++ clock project/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace clock_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblsecond = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Font = new System.Drawing.Font("Script MT Bold", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(150, 19);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(278, 117);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "22:22";
+            // 
+            // lblsecond
+            // 
+            this.lblsecond.AutoSize = true;
+            this.lblsecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblsecond.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblsecond.ForeColor = System.Drawing.Color.White;
+            this.lblsecond.Location = new System.Drawing.Point(410, 102);
+            this.lblsecond.Name = "lblsecond";
+            this.lblsecond.Size = new System.Drawing.Size(59, 46);
+            this.lblsecond.TabIndex = 1;
+            this.lblsecond.Text = "22";
+            this.lblsecond.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(636, 182);
+            this.Controls.Add(this.lblsecond);
+            this.Controls.Add(this.lblTime);
+            this.Font = new System.Drawing.Font("Algerian", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblsecond;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
